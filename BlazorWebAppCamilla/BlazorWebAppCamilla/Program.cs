d4b0c85b19af5d8c3d00bb096dc5d49c2fa53958
Camilla Björkgren <camilla_bjorkgren@hotmail.com>
2025-03-23 22:05:19 +0100

    changes in connection string etc

--- BlazorWebAppCamilla/BlazorWebAppCamilla/Program.cs
+++ BlazorWebAppCamilla/BlazorWebAppCamilla/Program.cs
@@ -71,7 +71,7 @@ builder.Services.AddAuthorization(x =>
     x.AddPolicy("Users", policy => policy.RequireRole("SuperAdmin", "CIO", "Admin", "User"));
 });
 
-builder.Services.AddSingleton(s => new GraphQLHttpClient("https://courseprovider-silicon-camilla.azurewebsites.net/api/graphql?code=LL18Cb5IbCut7M9hw0bCm5RFI7hOjOJwHOmU6-Dyd1TXAzFuSGQ4FA%3D%3D", new SystemTextJsonSerializer()));
+builder.Services.AddSingleton(s => new GraphQLHttpClient("***REMOVED***", new SystemTextJsonSerializer()));
 builder.Services.AddSingleton<IEmailSender<ApplicationUser>, IdentityNoOpEmailSender>();
 builder.Services.AddSignalR();
 
